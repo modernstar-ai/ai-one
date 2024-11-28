fc96502b38ffbe9d0efe3a6adaa495327b1a6f17
Yasir <yasir.fayrooz@hotmail.com>
2024-11-28 23:07:23 +1030

    chat settings

--- src/Agile.Chat/Framework/Agile.Framework.Ai/AppKernel.cs
+++ src/Agile.Chat/Framework/Agile.Framework.Ai/AppKernel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using Agile.Framework.Ai.Models;
 using Agile.Framework.Common.Enums;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
@@ -19,12 +20,17 @@ public class AppKernel(Kernel kernel)
 
      private async Task<ReadOnlyMemory<float>> GenerateEmbeddingAsync(string text) =>
          await _textEmbedding.GenerateEmbeddingAsync(text);
+     
+     private async Task<IList<ReadOnlyMemory<float>>> GenerateEmbeddingsAsync(IList<string> texts) =>
+         await _textEmbedding.GenerateEmbeddingsAsync(texts);
 
-     private async IAsyncEnumerable<Dictionary<ResponseType, string>> GetChatStream(ChatHistory chatHistory)
+     private async IAsyncEnumerable<Dictionary<ResponseType, string>> GetChatStream(ChatHistory chatHistory, ChatSettings settings)
      {
          var options = new AzureOpenAIPromptExecutionSettings()
          {
-            
+            Temperature = settings.Temperature,
+            TopP = settings.TopP,
+            MaxTokens = settings.MaxTokens
          };
          
          var responses = _chatCompletion.GetStreamingChatMessageContentsAsync(chatHistory, options, kernel);
---
+++ src/Agile.Chat/Framework/Agile.Framework.Ai/Models/ChatSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Agile.Framework.Ai.Models;
+
+public class ChatSettings
+{
+    public double? Temperature { get; set; }
+    public double? TopP { get; set; }
+    public int? MaxTokens { get; set; }
+}
