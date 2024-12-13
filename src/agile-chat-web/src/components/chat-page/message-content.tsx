cd95f763648b562ea3b25adfd47de4e2e963d643
Yasir <yasir.fayrooz@hotmail.com>
2024-12-13 13:30:00 +1030

    separating citations out from documents retrieved

--- src/agile-chat-web/src/components/chat-page/message-content.tsx
+++ src/agile-chat-web/src/components/chat-page/message-content.tsx
@@ -2,9 +2,10 @@ import { Markdown } from '@/components/markdown/markdown';
 import { Citation, Message } from '@/types/ChatThread';
 import { CitationSheet } from './citation-sheet';
 import { ChatSearchResponse } from './chat-search-response';
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { TempIdType } from '@/pages/Chat/utils';
 import useStreamStore from '@/stores/stream-store';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../ui/accordion';
 
 interface MessageContentProps {
   message: Message;
@@ -13,6 +14,14 @@ interface MessageContentProps {
 
 const MessageContent = (props: MessageContentProps) => {
   const { message, assistantId } = props;
+  const documents = (message.options.metadata.Citations as Citation[]) ?? undefined;
+
+  const citations = useMemo<Citation[] | undefined>(() => {
+    return documents?.filter((_, index) => {
+      const superscriptedIndex = toSuperscript(index + 1);
+      return message.content.includes(`⁽${superscriptedIndex}⁾`);
+    });
+  }, [documents]);
   const contentOverride = useRef<string | undefined>(undefined);
 
   useEffect(() => {
@@ -47,6 +56,27 @@ const MessageContent = (props: MessageContentProps) => {
     }
   };
 
+  function toSuperscript(number: number) {
+    const superscriptMap = {
+      '0': '⁰',
+      '1': '¹',
+      '2': '²',
+      '3': '³',
+      '4': '⁴',
+      '5': '⁵',
+      '6': '⁶',
+      '7': '⁷',
+      '8': '⁸',
+      '9': '⁹',
+    } as Record<string, string>;
+
+    return number
+      .toString()
+      .split('')
+      .map((digit) => superscriptMap[digit] || digit)
+      .join('');
+  }
+
   if (message.options.metadata.SearchProcess) {
     return <ChatSearchResponse message={message} assistantId={assistantId!} />;
   }
@@ -59,18 +89,61 @@ const MessageContent = (props: MessageContentProps) => {
       ></Markdown>
 
       {/* Handle citations */}
-      {message.options.metadata.Citations && (
-        <div className="citations">
-          <h5 className="font-bold">Citations:</h5>
-          {(message.options.metadata.Citations as Citation[]).map((citation, index) =>
-            citation.name && citation.url ? (
-              <CitationSheet index={index + 1} citation={citation} key={message.id + index} assistantId={assistantId} />
-            ) : (
-              <li key={index}>Invalid citation data</li>
-            )
-          )}
-        </div>
-      )}
+      <Accordion
+        type="single"
+        collapsible
+        className="m-0 p-0 not-prose"
+        defaultValue={citations?.length ?? 0 > 0 ? 'item-1' : ''}
+      >
+        {citations && citations.length > 0 && (
+          <AccordionItem value="item-1" className="m-0 p-0">
+            <AccordionTrigger className="m-0 p-0">Citations</AccordionTrigger>
+            <AccordionContent className="m-0 p-0">
+              {citations && (
+                <div className="citations mb-2">
+                  {citations.map((citation, index) =>
+                    citation.name && citation.url ? (
+                      <CitationSheet
+                        index={documents.indexOf(citation) + 1}
+                        citation={citation}
+                        key={message.id + index}
+                        assistantId={assistantId}
+                      />
+                    ) : (
+                      <li key={index}>Invalid citation data</li>
+                    )
+                  )}
+                </div>
+              )}
+            </AccordionContent>
+          </AccordionItem>
+        )}
+
+        {documents && documents.length > 0 && (
+          <AccordionItem value="item-2" className="m-0 p-0">
+            <AccordionTrigger className="m-0 p-0">Documents Retrieved</AccordionTrigger>
+            <AccordionContent className="m-0 p-0">
+              {documents && (
+                <div className="citations mb-2">
+                  {documents.map((document, index) =>
+                    document.name && document.url ? (
+                      <CitationSheet
+                        index={index + 1}
+                        citation={document}
+                        key={message.id + index}
+                        assistantId={assistantId}
+                      />
+                    ) : (
+                      <li key={index}>Invalid citation data</li>
+                    )
+                  )}
+                </div>
+              )}
+            </AccordionContent>
+          </AccordionItem>
+        )}
+      </Accordion>
+
       <div ref={scrollContainerRef}></div>
     </>
   );
