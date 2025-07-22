835922838565d24d7dd041c9e789efcb2aa961cb
Rakesh Lakshminarayana <rakeshl84@outlook.com>
2025-07-22 13:09:20 +1000

    update

--- src/Agile.Chat/Agile.Chat.Application/ChatCompletions/Dtos/ChatDto.cs
+++ src/Agile.Chat/Agile.Chat.Application/ChatCompletions/Dtos/ChatDto.cs
@@ -1,12 +1,8 @@
 ﻿using Agile.Chat.Domain.Assistants.Aggregates;
-using Agile.Chat.Domain.ChatThreads.Aggregates;
-
 namespace Agile.Chat.Application.ChatCompletions.Dtos;
 
 public class ChatDto
 {
     public string ThreadId { get; set; }
     public string UserPrompt { get; set; }
-    public ChatThread Thread { get; set; }
-    public Assistant Assistant { get; set; }
 }
